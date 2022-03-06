c356124ef700c6f9c925d05c59ec2ed55ff61a85
Cleriston Novaes <novaes.cleriston@gmail.com>
2022-03-06 13:18:59 -0300

    Finalizado

--- ivoryit-testeestagio-campo-minado-java-main/src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
+++ ivoryit-testeestagio-campo-minado-java-main/src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
@@ -1,5 +1,7 @@
 package com.ivory.TesteEstagio.CampoMinado;
 
+import java.util.Random;
+
 public class Program {
 	
 	public void executar() {
@@ -10,6 +12,27 @@ public class Program {
 		System.out.println(campoMinado.Tabuleiro());
 		
 		// Realize sua codificação a partir deste ponto, boa sorte!
+		Random rand = new Random();
+		int jogada = 0;
+		while(campoMinado.JogoStatus() == StatusTipo.Aberto) {
+			
+			int linha = rand.nextInt(8) + 1;
+			int coluna = rand.nextInt(8) + 1;
+			jogada++;
+			System.out.println("Jogo: " + jogada);
+			System.out.println("Jogada escolhida, linha " + linha + " e coluna " + coluna);
+			
+			campoMinado.Abrir(linha, coluna);
+			
+			System.out.println(campoMinado.Tabuleiro());
+		}
+		
+		if(campoMinado.JogoStatus() == StatusTipo.GameOver) {
+			System.out.println("Você perdeu!!!");
+		}
+		else if(campoMinado.JogoStatus() == StatusTipo.Vitoria) {
+			System.out.println("Parabéns, você venceu!!!");
+		}
 	}
 
 }
